fd4273cc67845a375df259ec6645773ee0566971
sakib7351294 <136421669+sakib7351294@users.noreply.github.com>
2025-08-27 17:41:10 +0600

    Update app.js

--- app.js
+++ app.js
@@ -4,27 +4,22 @@ let socket = require('socket.io');
 // create express app
 let app = express();
 
-// static files (public folder)
-app.use(express.static('public'));
+// static files
+app.use(express.static('./public'));
 
-// listen on Render's PORT or fallback 4500
+// listen on port 4500
 let PORT = process.env.PORT || 4500;
 let server = app.listen(PORT, () => {
     console.log("Server is running on port " + PORT);
 });
 
+
 // socket setup
 let io = socket(server);
 
-io.on('connection', (socket) => { 
-    console.log("New user connected");
+io.on('connection', (socket)=>{ 
 
-    socket.on('chat', (data) => {
-        io.sockets.emit('chat', data); 
-    });
-
-    socket.on('disconnect', () => {
-        console.log("User disconnected");
-    });
+      socket.on('chat', (data)=>{
+            io.sockets.emit('chat', data); 
+      });
 });
- 
